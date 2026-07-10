9ef34c5399aefea7ee0da7b550772916e24935d4
Bob <mr.bibekdahal@gmail.com>
2026-07-10 21:28:27 +0545

    different types of data types

---
+++ data_types.py
@@ -0,0 +1,20 @@
+a = 1         #integer
+b = 2.2       #float
+c = -3        #integer
+d = 4j        #complex
+e = 5e3       #float because e acts as exponetial which means power of 10
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+print(c)
+print(type(c))
+
+print(d)
+print(type(d))
+
+print(e)
+print(type(e))
