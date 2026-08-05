118544b1e0e38b011f197ffcc554d1bb6584a91a
Bob <mr.bibekdahal@gmail.com>
2026-08-05 20:19:29 +0545

    using match and adding case

---
+++ combine_value_match.py
@@ -0,0 +1,12 @@
+month  = 3
+
+match month:
+    case 3|4|5 :
+        print(" March April May")
+    case 6|7|8 :
+        print("June July August")
+    case 9|10|11 :
+        print("September October November")
+    case 12|1|2 :
+        print("December January Feburary")
+    
