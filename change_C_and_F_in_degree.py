f324ac3d144d223d3de73140fe72b100484c7ecd
Bob <mr.bibekdahal@gmail.com>
2026-08-19 18:44:43 +0545

    change c and f degree

---
+++ change_C_and_F_in_degree.py
@@ -0,0 +1,11 @@
+#change celsius to farhenheit
+celsius1 = 22
+farhrenheit1 = (celsius1 *9 /5 ) +32
+print(farhrenheit1)
+
+
+#change farhenheit to celsius
+
+# fahrenheit2 = 98
+# celsius2 = (fahrenheit2 - 32)*5/9
+# print(celsius2)
