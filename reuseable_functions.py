f5b4136f532154ce398bd74250a9784bea43373f
Bob <mr.bibekdahal@gmail.com>
2026-08-20 12:19:09 +0545

    functions and can be used again and again

---
+++ reuseable_functions.py
@@ -0,0 +1,14 @@
+def fahrenheit_to_celsius(fahrenheit):
+    return(fahrenheit - 32) *5 / 9
+
+print(fahrenheit_to_celsius(100))
+print(fahrenheit_to_celsius(98))
+print(fahrenheit_to_celsius(105))
+
+
+# #change celsius to fahrenheit
+# def celsius_to_fahrenheit(celsius):
+#     return((celsius *9 /5 ) +32)
+# print(celsius_to_fahrenheit(33))
+# print(celsius_to_fahrenheit(22))
+# print(celsius_to_fahrenheit(42))
